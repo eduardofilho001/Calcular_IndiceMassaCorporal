332e1f51902525f2093ecebaaab6818311aac694
Eduardo Filho <eduardosmfilho01@gmail.com>
2025-08-21 01:51:54 +0100

    adicionaei mensagem sobre o peso.

--- indiceMassaCorporal/views.py
+++ indiceMassaCorporal/views.py
@@ -12,6 +12,7 @@ def resultado(request):
     peso = None
     altura = None
     resultado = None
+    classificacao = None
     
     if request.method == "POST":
         form = FormIMC(request.POST)
@@ -20,9 +21,28 @@ def resultado(request):
             altura = form.cleaned_data["altura"]
             
             resultado = peso/(altura*altura)
+    
+            if resultado < 18.5: 
+                classificacao = "Abaixo do Peso"
+
+            elif 18.5 <= resultado < 25:
+                classificacao="Peso Normal"
+
+            elif 25 <= resultado < 30:
+                classificacao="Sobrepeso"
+
+            elif 30 <= resultado < 35: 
+                classificacao="Obesidade Grau 1"
+
+            elif 35 <= resultado < 40:
+                classificacao="Obesidade Grau 2"
+
+            else:
+                classificacao="Obesidade Grau 3"
 
     return render(request, 'resultado.html', {
         "peso":peso,
         "altura":altura,
-        "resultado":resultado
+        "resultado":resultado,
+        "classificacao":classificacao
     })
